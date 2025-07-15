15e841f5c1fa2516c60bf1039fb763d665662d15
yuvrajyadav05 <yuvrajyadav5octo@gmail.com>
2025-07-15 11:59:12 +0530

    Commit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,44 +1,109 @@
-import {User} from '../models/user.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-export const register=async(req,res)=>{
+import { User } from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import sendMail from "../middlewares/sendMail.js";
+import TryCatch from "../middlewares/TryCatch.js";
 
-    export const register = async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
+export const register = TryCatch(async (req, res) => {
+  const { email, name, password } = req.body;
 
-        let user = await User.findOne({ email });
+  let user = await User.findOne({ email });
 
-        if (user) {
-            return res.status(400).json({
-                message: "User already exists",
-            });
+  if (user)
+    return res.status(400).json({
+      message: "User Already exists",
+    });
 
-            const hashPassword = await bcrypt.hash(password, 10);  // Hash the password
+  const hashPassword = await bcrypt.hash(password, 10);
 
-            user = {
-                name,
-                email,
-                password: hashPassword // Hash the password 
-            };
+  user = {
+    name,
+    email,
+    password: hashPassword,
+  };
 
-            const otp = Math.floor(100000 + Math.random()); // Generate a 6-digit OTP
-            const activationToken = jwt.sign({ 
-                user,
-                otp
-            }, process.env.Activation_Secret,
-            {
-            expiresIn: '5m',
-         } // Set the token to expire in 15 minutes
-        );
+  const otp = Math.floor(Math.random() * 1000000);
 
-        const data={
-            user,
-            otp,
-        };
-    } catch (error) {
-        res.status(500).json({
-            message:error.message,
-        });
+  const activationToken = jwt.sign(
+    {
+      user,
+      otp,
+    },
+    process.env.Activation_Secret,
+    {
+      expiresIn: "5m",
     }
-};
+  );
+
+  const data = {
+    name,
+    otp,
+  };
+
+  await sendMail(email, "E learning", data);
+
+  res.status(200).json({
+    message: "Otp send to your mail",
+    activationToken,
+  });
+});
+
+export const verifyUser = TryCatch(async (req, res) => {
+  const { otp, activationToken } = req.body;
+
+  const verify = jwt.verify(activationToken, process.env.Activation_Secret);
+
+  if (!verify)
+    return res.status(400).json({
+      message: "Otp Expired",
+    });
+
+  if (verify.otp !== otp)
+    return res.status(400).json({
+      message: "Wrong Otp",
+    });
+
+  await User.create({
+    name: verify.user.name,
+    email: verify.user.email,
+    password: verify.user.password,
+  });
+
+  res.json({
+    message: "User Registered",
+  });
+});
+
+export const loginUser = TryCatch(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user)
+    return res.status(400).json({
+      message: "No User with this email",
+    });
+
+  const mathPassword = await bcrypt.compare(password, user.password);
+
+  if (!mathPassword)
+    return res.status(400).json({
+      message: "wrong Password",
+    });
+
+  const token = jwt.sign({ _id: user._id }, process.env.Jwt_Sec, {
+    expiresIn: "15d",
+  });
+
+  res.json({
+    message: `Welcome back ${user.name}`,
+    token,
+    user,
+  });
+});
+
+export const myProfile = TryCatch(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  res.json({ user });
+});
